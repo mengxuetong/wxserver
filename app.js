a8129db455e3a3f961faba14722ddfc47b924868
mengxuetong <mengxuetong@foxmail.com>
2015-11-04 17:28:55 +0800

    all

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var http = require('http'),
     express = require('express'),
     jsSHA = require('jssha');
 var app = express();
-app.all("*",allRequest);
+var jsapi_ticket,noncestr,timestamp,url;
 
-function allRequest(req,res){
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+app.all("/token",allRequest);
+
+function allRequest(req,res,next){
   //检验 token
   wechat.checkSignature(req, res);
   //预处理
@@ -55,6 +66,69 @@ function allRequest(req,res){
     wechat.send(msg);
   });
 }
+
+app.get('/jssdk',function(req,res){
+  console.log('enter jssdk')
+  http.get('http://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxdd42174ce7038c28&secret=d4624c36b6795d1d99dcf0547af5443d', function(_res) {
+   // 这个异步回调里可以获取access_token
+   console.log(_res);
+   var access_token = _res.access_token;
+   
+   http.get('http://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=access_token&type=jsapi', function(_res){
+     // 这个异步回调里可以获取ticket
+     console.log(_res)
+     jsapi_ticket = _res.ticket;
+     var createNonceStr = function () {
+        return Math.random().toString(36).substr(2, 15);
+      };
+
+      var createTimestamp = function () {
+        return parseInt(new Date().getTime() / 1000) + '';
+      };
+
+      var raw = function (args) {
+        var keys = Object.keys(args);
+        keys = keys.sort()
+        var newArgs = {};
+        keys.forEach(function (key) {
+          newArgs[key.toLowerCase()] = args[key];
+        });
+
+        var string = '';
+        for (var k in newArgs) {
+          string += '&' + k + '=' + newArgs[k];
+        }
+        string = string.substr(1);
+        return string;
+      };
+
+      /**
+      * @synopsis 签名算法 
+      *
+      * @param jsapi_ticket 用于签名的 jsapi_ticket
+      * @param url 用于签名的 url ，注意必须动态获取，不能 hardcode
+      *
+      * @returns
+      */
+      var sign = function (jsapi_ticket, url) {
+        var ret = {
+          jsapi_ticket: jsapi_ticket,
+          nonceStr: createNonceStr(),
+          timestamp: createTimestamp(),
+          url: url
+        };
+        var string = raw(ret);
+            jsSHA = require('jssha');
+            shaObj = new jsSHA(string, 'TEXT');
+        ret.signature = shaObj.getHash('SHA-1', 'HEX');
+
+        return ret;
+      };
+      res.json(sign());
+      //module.exports = sign;
+    });
+  });
+})
 app.listen(3030,function(){
   console.log('Example app listening at port 3030');
 })
