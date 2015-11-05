b57deb5dd410edc4bcc2a1cd529a9fbbe96470d9
mengxuetong <mengxuetong@foxmail.com>
2015-11-05 14:17:20 +0800

    df‘

--- routes/jssdk.js
+++ routes/jssdk.js
@@ -120,7 +120,6 @@ var bodyParser = require('body-parser');
 	router.post('/jssdk/:index', function(req, res) {
 
 		var index = req.params.index;
-		console.log(req.body)
 		var _url = req.body.url;
 
 		var signatureObj = cachedSignatures[_url];
@@ -148,8 +147,6 @@ var bodyParser = require('body-parser');
 			// 此处可能需要清理缓存当中已过期的数据
 		}
 
-		console.log(appIds);
-console.log(appIds[index]);
 		// 获取微信签名所需的access_token
 		http.get('http://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+ appIds[index].appid +'&secret=' + appIds[index].secret, function(_res) {
 			var str = '';
