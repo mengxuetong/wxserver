647c85ae53160c422559413af6fcb196ef90b32e
mengxuetong <mengxuetong@foxmail.com>
2015-11-05 11:25:06 +0800

    all

--- wxconfig.js
+++ wxconfig.js
@@ -1,12 +1,12 @@
 module.exports = function(){
 	return [
 			{
-				appid: 'wx211e56118677xxxx'
-				,secret: 'e2f63d171ac1b3170cf44920872fxxxx'
+				appid: 'wxc5e5f0f7bd8d4f22'
+				,secret: 'e0302a0f90d9d154de9c5cf13547fcac'
 			}
 			,{
-				appid: 'wxa0f06601f194xxxx'
-				,secret: '097fd14bac218d0fb016d02f525dxxxx'
+				appid: 'wxdd42174ce7038c28'
+				,secret: 'd4624c36b6795d1d99dcf0547af5443d'
 			}
 		];
 };
