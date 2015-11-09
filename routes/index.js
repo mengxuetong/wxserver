c1f9db41c871d0ac2420fe42d1422f3b0b636a02
mengxuetong <mengxuetong@foxmail.com>
2015-11-09 17:13:25 +0800

    all

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ var express = require('express');
 	router = express.Router();
 
 module.exports = router.get('/',function(req, res){
-	res.render('index',{title: 'express'});
+	res.render('index',{title: 'Node微信jssdk服务器'});
 })
